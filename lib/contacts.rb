bb35907373b42033b22bed62218614d944bf6f7a
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-08 17:39:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person == "Freddy Mercury"
-      data.each do |key, value|
+      data.collect do |key, value|
         if key == "favorite_ice_cream_flavors"
           value.delete_at(value.index("strawberry"))
         end 
